bc4be1bc7295783b91a01ba5b5fd57a18707a7ff
Omar Salem <omar@omarsalempro.com>
2026-07-07 13:55:59 -0600

    Use decorator manuall

--- decorator.py
+++ decorator.py
@@ -1,12 +1,22 @@
 import time
 
+def timer_dec(base_fn):
+    def enhanced_fn():
+        start_time = time.time()
+        base_fn()
+        end_time = time.time()
+        print(f"Function execution time: {end_time - start_time} seconds")
+    return enhanced_fn
+
+
+#@timer_dec    
 def brew_tea():
     # Code to brew
-    start_time = time.time()
     print("Brewing tea...")
     time.sleep(1)
-    print("Tea is ready!")  
-    end_time = time.time()
-    print(f"Tea brewing time: {end_time - start_time} seconds")
+    print("Tea is ready!")    
     
-brew_tea()  
+ dec_brew_tea = timer_dec(brew_tea) 
+ dec_brew_tea()
+
+#brew_tea()
